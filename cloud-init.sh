c02aa14024a82f61d3e5642873b96ff44997de9c
FernandoVR <fvr.dev@gmail.com>
2024-05-12 19:37:52 +0000

    chore: add sudo to docker compose installation

--- cloud-init.sh
+++ cloud-init.sh
@@ -7,6 +7,6 @@ curl -fsSL https://get.docker.com -o install-docker.sh &&\
     sudo systemctl start docker
 
 # Install Docker Compose
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
