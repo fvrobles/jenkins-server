ba9a2a676d1a4ec0a3ab7c032decf48e4a9ce799
FernandoVR <fvr.dev@gmail.com>
2024-05-12 19:04:45 +0000

    chore: fix init script

--- cloud-init.sh
+++ cloud-init.sh
@@ -3,4 +3,4 @@ curl -fsSL https://get.docker.com -o install-docker.sh &&\
     sudo sh install-docker.sh &&\
     sh -c 'sudo usermod -aG docker $USER' &&\
     sudo systemctl enable docker &&\
-    sudo systemctl start docker &&
+    sudo systemctl start docker
