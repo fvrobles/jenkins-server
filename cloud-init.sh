5fb54fc3f76c1c17ba06632822d7299c968e1113
FernandoVR <fvr.dev@gmail.com>
2024-05-12 18:53:47 +0000

    ci: create Jenkins server using terraform

---
+++ cloud-init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -fsSL https://get.docker.com -o install-docker.sh &&\
+    sudo sh install-docker.sh &&\
+    sh -c 'sudo usermod -aG docker $USER' &&\
+    sudo systemctl enable docker &&\
+    sudo systemctl start docker &&
---
+++ main.tf
@@ -0,0 +1,159 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "bbf9bbd1-5581-4a66-89c8-f128b7e41288"
+  tenant_id       = "97e17075-cc4f-4b3e-8ed4-476a32f24578"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "resourceGroup"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "virtualNetwork"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "vmPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vmNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  
+  security_rule {
+    name                       = "Jenkins5000"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "App8080"
+    priority                   = 1005
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "tls_private_key" "admin_ssh_key" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+    name                  = "vm-ubuntu-jenkins"
+    resource_group_name   = azurerm_resource_group.rg.name
+    location              = azurerm_resource_group.rg.location
+    size                  = "Standard_B1s" # Free tier eligible
+    admin_username        = "adminuser"
+    network_interface_ids = [azurerm_network_interface.nic.id]
+    custom_data           = base64encode(file("cloud-config.yml"))
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+      offer                 = "0001-com-ubuntu-server-jammy"
+      publisher             = "Canonical"
+      sku                   = "22_04-lts-gen2"
+      version               = "latest"
+    }
+
+    admin_ssh_key {
+        username   = "adminuser"
+        public_key = tls_private_key.admin_ssh_key.public_key_openssh
+    }
+
+}
